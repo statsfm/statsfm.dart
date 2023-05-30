96a45ae7c7709f57001e4a16c98c9444f7184f03
Elias <elias@isontic.com>
2023-05-30 13:04:05 -0700

    chore: update dependencies

--- lib/statsfm.dart
+++ lib/statsfm.dart
@@ -3,11 +3,7 @@ library statsfm;
 import 'dart:async';
 import 'dart:collection';
 import 'package:dio/dio.dart';
-import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
-import 'package:dio_cache_interceptor_hive_store/dio_cache_interceptor_hive_store.dart';
 import 'package:dio_smart_retry/dio_smart_retry.dart';
-import 'package:path_provider/path_provider.dart';
-
 import 'src/models/_models.dart';
 
 export 'src/models/_models.dart';
