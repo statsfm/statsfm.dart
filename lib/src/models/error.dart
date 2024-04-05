27344d66b82443647caa3ab24d1f4f31c345a2e2
Elias <elias@deuss.dev>
2024-04-05 11:02:46 +0200

    feat: export error model

--- lib/src/models/error.dart
+++ lib/src/models/error.dart
@@ -20,3 +20,18 @@ class StatsfmError extends Object {
   @JsonKey(name: 'message')
   late String message;
 }
+
+
+class StatsfmException implements Exception {
+  int status;
+  String message;
+
+  StatsfmException(this.status, this.message);
+
+  StatsfmException.fromStatsfm(StatsfmError error)
+      : status = error.status,
+        message = error.message;
+
+  @override
+  String toString() => 'Error Code: $status\r\n$message';
+}
--- lib/src/statsfm_exception.dart
+++ lib/src/statsfm_exception.dart
@@ -1,19 +1,5 @@
 part of statsfm;
 
-class StatsfmException implements Exception {
-  int status;
-  String message;
-
-  StatsfmException(this.status, this.message);
-
-  StatsfmException.fromStatsfm(StatsfmError error)
-      : status = error.status,
-        message = error.message;
-
-  @override
-  String toString() => 'Error Code: $status\r\n$message';
-}
-
 class ApiRateException extends StatsfmException {
   final num retryAfter;
 
